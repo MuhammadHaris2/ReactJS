aeb0746c5cce27e03b73b4e97409fd33932885b1
haris <qaswar1972@gmail.com>
2020-08-20 14:10:50 +0500

    Updates

---
+++ precache-manifest.d8a7772b0216f7bf74999dc7c856c61e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e62522d89974173e695031ec7d404387",
+    "url": "/ReactJS/index.html"
+  },
+  {
+    "revision": "0a5a95da0104db9298cc",
+    "url": "/ReactJS/static/css/main.f9c8815c.chunk.css"
+  },
+  {
+    "revision": "82280cfe7d5fd551f497",
+    "url": "/ReactJS/static/js/2.1d42db15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJS/static/js/2.1d42db15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a5a95da0104db9298cc",
+    "url": "/ReactJS/static/js/main.d77ed0c6.chunk.js"
+  },
+  {
+    "revision": "66c977a9826db0a53ca3",
+    "url": "/ReactJS/static/js/runtime-main.a8078f87.js"
+  }
+]);

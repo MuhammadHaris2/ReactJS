e207e47eaac65276fd04e1ebf22638fea78cb182
haris <qaswar1972@gmail.com>
2020-08-20 14:19:53 +0500

    fst

--- src/Fdata.jsx
+++ src/Fdata.jsx
@@ -19,17 +19,17 @@ const Fdata = [
       
     },
     {
-        imgsrc : "https://stat2.bollywoodhungama.in/wp-content/uploads/2019/09/Lootcase-2.jpg",
-        sname : "Comedy",
-        title :  "Lootcase",
-        link : "http://funtime.net.pk/en/details/movies/16038"
+        imgsrc : "https://static.toiimg.com/photo/msid-76624099/76624099.jpg?378969",
+        sname : "Comedy, Drama, Romance",
+        title :  "Dil Bechara",
+        link : "http://funtime.net.pk/en/details/movies/16011"
       
     },
     {
-        imgsrc : "https://stat2.bollywoodhungama.in/wp-content/uploads/2019/09/Lootcase-2.jpg",
-        sname : "Comedy",
-        title :  "Lootcase",
-        link : "http://funtime.net.pk/en/details/movies/16038"
+        imgsrc : "https://wallpapercave.com/wp/wp6373216.jpg",
+        sname : "Drama, Romance",
+        title :  "Love Aaj Kal",
+        link : "http://funtime.net.pk/en/details/movies/15579"
       
     }
 ];
